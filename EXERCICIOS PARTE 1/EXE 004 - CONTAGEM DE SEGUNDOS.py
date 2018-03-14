c84062248760eca8952ebe7f97bcaf00e4fa6eea
Luiz-Ar <37367902+Luiz-Ar@users.noreply.github.com>
2018-03-14 11:13:03 -0300

    Converter segundos
    Converter segundos para dias, horas, minutos e segundos.

--- EXERCICIOS PARTE 1/EXE 004 - CONTAGEM DE SEGUNDOS.py
+++ EXERCICIOS PARTE 1/EXE 004 - CONTAGEM DE SEGUNDOS.py
@@ -1,11 +1,9 @@
 seg = input("Por favor, entre com o número de segundos que deseja converter: ")
 segTotal = int(seg)
-
 horas = segTotal // 3600
 segrestantes = segTotal % 3600
 dias = horas // 24
 horasRest = horas % 24
 minutos = segrestantes // 60
 segundos = segrestantes % 60
-
 print(dias, "dias,", horasRest, "horas,", minutos, "minutos e", segundos, "segundos.")
