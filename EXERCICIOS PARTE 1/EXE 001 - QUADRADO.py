f5fbae22426b64adce04d4e9f77c09f158fb3044
Luiz-Ar <37367902+Luiz-Ar@users.noreply.github.com>
2018-03-14 11:08:49 -0300

    Perímetro e área do Quadrado

--- EXERCICIOS PARTE 1/EXE 001 - QUADRADO.py
+++ EXERCICIOS PARTE 1/EXE 001 - QUADRADO.py
@@ -1,6 +1,6 @@
-lado = input("Digite o valor correspondente ao lado de um quadrado: ")
+lad = input("Digite o valor correspondente ao lado de um quadrado: ")
 
-perímetro = int(lado) * 4
-área = int(lado) * int(lado)
+perímetro = int(lad) * 4
+área = int(lad) * int(lad)
 
 print("perímetro:", perímetro," - área:", área)
