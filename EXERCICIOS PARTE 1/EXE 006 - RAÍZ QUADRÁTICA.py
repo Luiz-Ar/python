fe8b38ddb73941560e6094f01227133739c1e914
Luiz-Ar <37367902+Luiz-Ar@users.noreply.github.com>
2018-03-14 11:15:02 -0300

    Função de 2º Grau
    Encontrar raízes de 2º grau.

--- EXERCICIOS PARTE 1/EXE 006 - RAÍZ QUADRÁTICA.py
+++ EXERCICIOS PARTE 1/EXE 006 - RAÍZ QUADRÁTICA.py
@@ -3,6 +3,7 @@ a = float(input("Entre o valor de a: "))
 b = float(input("Entre o valor de b: "))
 c = float(input("Entre o valor de c: "))
 delta = b ** 2 - 4 * a * c
+
 if delta >= 0:
     x1 = (- b + math.sqrt(delta)) / (2 * a)
     x2 = (- b - math.sqrt(delta)) / (2 * a)
